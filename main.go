0cc2dfafc7a239939776943b86a5bd56232c2f68
Lindemberg-Chagas <lindembergchagas@hotmail.com>
2023-07-21 14:30:01 -0300

    Quarta parte do codigo Banco / Func Pagar boleto usando a Interface como contrato para 2 Structs Conta Corrente e Poupança

--- main.go
+++ main.go
@@ -6,12 +6,20 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta VerficaConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type VerficaConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaLindemberg := contas.ContaCorrente{Titular: clientes.Titular{Nome: "Lindemberg", CPF: "123.123.123-12"}}
 
 	fmt.Println(contaLindemberg)
 	fmt.Println(contaLindemberg.ObterSaldo())
 	contaLindemberg.Depositar(100)
-	fmt.Println(contaLindemberg.ObterSaldo())
+	PagarBoleto(&contaLindemberg, 40)
 
 }
